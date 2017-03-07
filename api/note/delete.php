087119d37fd4701ce1c9d31d0b65c2b7ba279474
bergwhite <bergwhites@gmail.com>
2017-03-07 23:57:04 +0800

    API的文章删除模块完成

--- api/note/delete.php
+++ api/note/delete.php
@@ -3,4 +3,27 @@
 include '../../conn.php';  // 引入数据库连接页面
 include '../user/login_check.php';  // 引入登陆检查页面，未登录不执行sql语句
 
+$cookie_userId = (int)$_COOKIE["userId"];  // 获取用户ID
+// echo gettype($cookie_userId);
+
+//$noteId = $_POST["noteId"];  // 获取文章ID
+$noteId = 29;
+
+/* 执行添加语句 */
+
+$sql = "delete from note where user_id = $cookie_userId and note_id = $noteId";
+$result = mysql_query($sql);
+
+/* 返回JSON状态*/
+
+if(mysql_affected_rows()){
+	$resultJSON = array("modifyState"=>"yes");
+}
+else {
+	$resultJSON = array("modifyState"=>"no");
+}
+echo json_encode($resultJSON);
+
+mysql_close($conn);  // 断开数据库连接
+
  ?>
