2539d6e5b14d4d7f30c5e6218a806a3b663da02a
bergwhite <bergwhites@gmail.com>
2017-03-08 18:51:20 +0800

    '增加注册和登陆框'

--- ajax.js
+++ ajax.js
@@ -7,25 +7,37 @@ var getJSON = (method,url) => {
 			notes.empty();
 			 console.log(result); // test if ajax works
 			if (result.loginState !== 'no') {
-				for (var x in result) {
-					let id = result[x].id;
-					let title = result[x].title;
-					let content = result[x].content;
-					let template = `
-						<div class="row note" id="${id}">
-							<div class="note-title">
-								<p class="text-center">${title}</p>
-								<div class="note-control">
-									<span class="glyphicon glyphicon-pencil"></span>
-									<span class="glyphicon glyphicon-trash"></span>
+				if (result[0].id!=='undefined') {
+					for (var x in result) {
+						let id = result[x].id;
+						let title = result[x].title;
+						let content = result[x].content;
+						let template = `
+							<div class="row note" id="${id}">
+								<div class="note-title">
+									<p class="text-center">${title}</p>
+									<div class="note-control">
+										<span class="glyphicon glyphicon-pencil"></span>
+										<span class="glyphicon glyphicon-trash"></span>
+									</div>
 								</div>
+								<div class="note-content">${content}</div>
 							</div>
-							<div class="note-content">${content}</div>
-						</div>
-					`;
+						`;
+						notes.append(template);
+					};
+				}
+				else {
+					let template = '<p class="note text-center">没有发现笔记哦，试着去添加一条笔记吧 ^_^</p>'
 					notes.append(template);
-				};
-			};
+				}
+				
+			}
+			else {
+				let template = '<p class="note text-center">没有发现笔记哦，请登陆 ^_^</p>'
+				notes.append(template);
+			}
+			;
 			
 			
 		};
